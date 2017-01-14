d2bce76783beb28759c937dd2b08aa904d434a7e
narijeong <narijeong@icloud.com>
2017-01-14 08:10:58 -0600

    add plotly lib and a graph

--- custom.js
+++ custom.js
@@ -1,15 +1,26 @@
 window.onload = function () {
-      google.charts.load('current', {'packages':['corechart']});
+      google.charts.load('current', {'packages':['corechart', 'line']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['x', 'temperature'],
-          ['1',  25],
-          ['2',  25],
-          ['3',  26],
-          ['4',  27]
-        ]);
+        var data = new google.visualization.DataTable();
+          data.addColumn('number', 'temperature');
+          data.addRows([
+            ['1',  25],
+            ['2',  25],
+            ['3',  26],
+            ['4',  26],
+            ['5',  27],
+            ['6',  25],
+            ['7',  25],
+            ['8',  26],
+            ['9',  25],
+            ['10',  27],
+            ['11',  26],
+            ['12',  27],
+            ['13',  25],
+            ['14',  23]
+          ]);
 
         var options = {
           legend: 'none',
